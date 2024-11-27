11239bf6bb624a947a60b65a399b97d9b2a1432c
k1sales <0000113314861xsp@al.educacao.sp.gov.br>
2024-11-27 16:40:24 -0300

    projeto-final

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Cinema',
+    'Em qual filme de animação os personagens principais são monstros que trabalham em uma fábrica de sustos?',
+    'Monstros S.A.'
+)
+
+criaCartao(
+    'Séries',
+    'Qual série da Netflix segue a vida de uma jovem que descobre que tem poderes telecinéticos e enfrenta um mundo sobrenaturais?',
+    'Stranger Things'
+)
+
+criaCartao(
+    'Desenhos Animados',
+    'Qual é o nome do personagem principal que mora em uma casa abacaxi no fundo do mar?',
+    'Bob Esponja'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual filme conta a história de um robô solitário que limpa a Terra e se apaixona por outro robô chamado EVA?',
+    'Wall-E'
+)
+
+criaCartao(
+    'Séries',
+    'Qual série tem como tema principal um grupo de zumbis que ameaça os sobreviventes em um mundo pós-apocalíptico?',
+    'The Walking Dead'
+)
